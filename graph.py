4d3e0d392830812769c2b8787e397d9818e38aab
Alexei Bobko <abvbobko@gmail.com>
2020-10-22 19:32:27 +0300

    Add request class and time tracking

--- graph.py
+++ graph.py
@@ -1,18 +1,38 @@
 import random
 
 
+class Request:
+    def __init__(self):
+        self.time = 0
+
+    def tact(self):
+        self.time += 1
+
+
 class ServiceLine:
     def __init__(self):
-        self.free_status = 0
+        # self.free_status = 0
+        self.request = None
 
-    def give_work(self):
-        self.free_status = 1
+    def give_work(self, request):
+        # self.free_status = 1
+        if not self.request:
+            self.request = request
+        else:
+            raise Exception("Service line is busy!")
 
     def free(self):
-        self.free_status = 0
+        self.request = None
+        # self.free_status = 0
 
     def get_status(self):
-        return self.free_status
+        if self.request:
+            return 1
+        return 0
+
+    def tact(self):
+        if self.request:
+            self.request.tact()
 
 
 class Queue:
@@ -20,22 +40,21 @@ class Queue:
         self.max_size = size
         self.queue = []
 
-    def push(self):
+    def push(self, request):
         if len(self.queue) < self.max_size:
-            self.queue = [1] + self.queue
+            self.queue = [request] + self.queue
             return True
         return False
 
     def pop(self):
-        self.queue.pop()
-
-    def has_free_cell(self):
-        if len(self.queue) == self.max_size:
-            return False
-        return True
+        return self.queue.pop()
 
     def get_current_size(self):
         return len(self.queue)
+
+    def tact(self):
+        for request in self.queue:
+            request.tact()
 
 
 class QueueingSystem:
@@ -65,6 +84,9 @@ class QueueingSystem:
         sl_1_status, sl_2_status = self.get_service_lines_statuses([self.service_line_1, self.service_line_2])
         # sl_1_status = self.service_line_1.get_status()
         # sl_2_status = self.service_line_2.get_status()
+        self.queue.tact()
+        self.service_line_1.tact()
+        self.service_line_2.tact()
         if sl_1_status == 1 and self.event(1 - self.p1):
             self.service_line_1.free()
             sl_1_status = 0
@@ -80,33 +102,29 @@ class QueueingSystem:
             pass
         elif queue_size == 1:
             if sl_1_status == 0:
-                self.service_line_1.give_work()
+                self.service_line_1.give_work(self.queue.pop())
                 sl_1_status = 1
-                self.queue.pop()
             elif sl_2_status == 0:
-                self.service_line_2.give_work()
+                self.service_line_2.give_work(self.queue.pop())
                 sl_2_status = 1
-                self.queue.pop()
         elif queue_size == 2:
             if sl_1_status == 0:
-                self.service_line_1.give_work()
+                self.service_line_1.give_work(self.queue.pop())
                 sl_1_status = 1
-                self.queue.pop()
             if sl_2_status == 0:
-                self.service_line_2.give_work()
+                self.service_line_2.give_work(self.queue.pop())
                 sl_2_status = 1
-                self.queue.pop()
 
-        if request == 1:
+        if request:
             if sl_1_status == 0:
-                self.service_line_1.give_work()
+                self.service_line_1.give_work(request)
                 # is_request_serviced = True
                 sl_1_status = 1
             elif sl_2_status == 0:
-                self.service_line_2.give_work()
+                self.service_line_2.give_work(request)
                 # is_request_serviced = True
                 sl_2_status = 1
-            elif not self.queue.push():
+            elif not self.queue.push(request):
                 is_rejected = True
                 # is_request_serviced = True
 
@@ -119,5 +137,5 @@ class QueueingSystem:
 
     def source(self):
         if self.event(self.ro):
-            return 0
-        return 1
+            return None
+        return Request()
